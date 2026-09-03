3c466c43bdd830f42d4f99d78f2394a8286d5d2c
Wafa Adam <wafaadam548@gmail.com>
2026-09-03 21:24:29 +0000

    Making the last nam eand the first name and the email and the pass

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 class ScreenManager:
     def __init__(self):
-      self.sign_in = input("Enter your username: ")
-      sing_in=self.sign_in  
+      self.f_name = input("Enter your First name : ")
+      First_name=self.f_name 
+      self.L_name = input("Enter your Last name : ")
+      Last_name=self.L_name
+      self.email = input("Enter your Email : ")
+      Pass=self.email
+      self.Pass = input("Enter your Pass name : ")
+      First_name=self.Pass  
       
 scren=ScreenManager()
 print(scren.sign_in)
