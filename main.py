7e9f7df5b07851750086039edf1ddc0d9da6d2c3
Wafa Adam <wafaadam548@gmail.com>
2026-09-03 21:19:28 +0000

    trying to opeen it in new labtop

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ class ScreenManager:
     def __init__(self):
       self.sign_in = input("Enter your username: ")
       sing_in=self.sign_in  
-
+      
 scren=ScreenManager()
-print(scren.sign_in
-)
+print(scren.sign_in)
+    
